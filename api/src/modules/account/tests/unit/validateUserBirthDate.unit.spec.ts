6ecd4185792609ae03afb594f86550247baa4205
murilo <murilohenriquematias@gmail.com>
2021-09-28 18:53:31 -0300

    better unit testing for validateUserBirthDate function

--- api/src/modules/account/tests/unit/validateUserBirthDate.unit.spec.ts
+++ api/src/modules/account/tests/unit/validateUserBirthDate.unit.spec.ts
@@ -7,4 +7,10 @@ describe('Function: validateUserBirthDate', () => {
     const birthDate = new Date();
     expect(() => { validateUserBirthDate(birthDate); }).to.throw('You must be 14 or older to sign up');
   });
+
+  it('should not throw error when user is more than 14 years old', () => {
+    const birthDate = new Date();
+    birthDate.setFullYear(new Date().getFullYear() - 14);
+    expect(() => { validateUserBirthDate(birthDate); }).not.to.throw();
+  });
 });
