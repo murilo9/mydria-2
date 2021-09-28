84b3e835b33a814cd78f44735b994e17ceec8c5f
murilo <murilohenriquematias@gmail.com>
2021-09-28 19:36:45 -0300

    clear code at sendMail function

--- api/src/modules/account/functions/sendMail.ts
+++ api/src/modules/account/functions/sendMail.ts
@@ -1,20 +1,13 @@
 import mailjetService from 'node-mailjet';
 import { MailjetMessage } from '../types';
 
-export default async function sendMail(message: MailjetMessage) {
+export default async function sendMail(message: MailjetMessage): Promise<void> {
   const mailjet = mailjetService.connect(process.env.MAILJET_TOKEN_A, process.env.MAILJET_TOKEN_B);
-  const request = mailjet
+  await mailjet
     .post('send', { version: 'v3.1' })
     .request({
       Messages: [
         message,
       ],
-    });
-  request
-    .then((result) => {
-      console.log(result.body);
-    })
-    .catch((err) => {
-      console.log(err.statusCode);
     });
 }
