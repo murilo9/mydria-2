313414312e057a54894e61ebda638f3643d1f6b6
murilo <murilohenriquematias@gmail.com>
2021-11-18 20:53:57 -0300

    simplified test/development environment

--- api/src/modules/mongo/functions/getClient.ts
+++ api/src/modules/mongo/functions/getClient.ts
@@ -2,9 +2,9 @@ import { MongoClient } from 'mongodb';
 import Result from '../../system/types/Result';
 import writeLog from '../../logging/functions/writeLog';
 
-const PORT = process.env.MONGODB_CONNECTION_PORT || '27017';
-const DB_NAME = process.env.MONGODB_DATABASE || 'mydria_db';
-const USER = process.env.MONGODB_USERNAME || 'mydria';
+const PORT = process.env.MONGODB_CONNECTION_PORT;
+const DB_NAME = process.env.MONGODB_DATABASE;
+const USER = process.env.MONGODB_USERNAME;
 const PASSWORD = process.env.MONGODB_PASSWORD;
 
 export default async function getClient(): Promise<Result<MongoClient>> {
