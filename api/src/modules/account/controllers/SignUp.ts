929a40ef78f3679f75a3f52b6c0fc78215607173
murilo <murilohenriquematias@gmail.com>
2021-11-21 17:57:24 -0300

    SignUp controller names fix

--- api/src/modules/account/controllers/SignUp.ts
+++ api/src/modules/account/controllers/SignUp.ts
@@ -2,8 +2,8 @@ import Controller from '../../system/types/Controller';
 import IAssertiveController from '../../system/types/IAssertiveController';
 import Result from '../../system/types/Result';
 import ResultAsyncFunction from '../../system/types/ResultAsyncFunction';
-import inserUserOnDatabase from '../DAL/insertUserOnDatabase';
-import inserUserPasswordOnDatabase from '../DAL/insertUserPasswordOnDb';
+import insertUserOnDatabase from '../DAL/insertUserOnDatabase';
+import insertUserPasswordOnDatabase from '../DAL/insertUserPasswordOnDb';
 import getPasswordHash from '../domain/getPasswordHash';
 import { SignUpRequest } from '../types/SignUpRequest';
 
@@ -16,16 +16,16 @@ export default class SignUpController extends Controller implements IAssertiveCo
   }
 
   async handle(request: SignUpRequest): Promise<Result<any>> {
-    // Validate form
+    // TODO: collect formatted form data from request body
     const signUpForm = { ...request.body };
     // Insert user on database
-    const createUserResult = await inserUserOnDatabase(signUpForm);
+    const createUserResult = await insertUserOnDatabase(signUpForm);
     if (createUserResult.failed) {
       return createUserResult;
     }
     // Insert user password on database
     const passwordHash = await getPasswordHash(signUpForm.password);
-    const createPasswordResult = await inserUserPasswordOnDatabase(passwordHash, createUserResult.payload._id);
+    const createPasswordResult = await insertUserPasswordOnDatabase(passwordHash, createUserResult.payload._id);
     if (createPasswordResult.failed) {
       return createPasswordResult;
     }
