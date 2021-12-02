85ba2c00390331c15d6474448f9952a1ec14b965
murilo <murilohenriquematias@gmail.com>
2021-12-01 22:32:34 -0300

    signin before create-comment e2e test

--- api/src/modules/comment/tests/e2e/create-comment.e2e.spec.ts
+++ api/src/modules/comment/tests/e2e/create-comment.e2e.spec.ts
@@ -6,17 +6,31 @@ import MydriaApp from '../../../../App';
 chai.use(chaiHttp);
 
 describe('Flow: create comment', () => {
+  const { app } = new MydriaApp();
+  const postId = 'noPostIdWasSetForCurrentTest'
+  let xAccessToken;
+
+  before(async () => {
+    const signinForm = {
+      email: 'jane.doe@email.com',
+      password: 'janepass123',
+    }
+    const loginRequest = await chai.request(app)
+      .post('/signin').send(signinForm)
+    xAccessToken = loginRequest.text;
+  })
+
   xit('should create comment on post', async () => {
     const { app } = new MydriaApp();
-    const postId = 'noPostIdWasSetOnCurrentTest'
-    const userId = 'noUserIdWasSetOnCurrentTest'
     const postForm = {
       body: {
         text: 'Hmmmm THIS is an interesting post!',
       },
     };
     const res = await chai.request(app)
-      .post(`/post/${postId}/${userId}`).send(postForm);
+      .post(`/post/${postId}/comments`)
+      .set('x-access-token', xAccessToken)
+      .send(postForm);
     expect(res).to.have.status(201);
   });
 });
