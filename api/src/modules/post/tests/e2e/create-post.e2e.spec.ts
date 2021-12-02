66f68f5dca3c4a7ae16bcae16c4fc5aa19b1fa53
murilo <murilohenriquematias@gmail.com>
2021-12-01 22:32:19 -0300

    signin before create-post e2e test

--- api/src/modules/post/tests/e2e/create-post.e2e.spec.ts
+++ api/src/modules/post/tests/e2e/create-post.e2e.spec.ts
@@ -6,16 +6,29 @@ import MydriaApp from '../../../../App';
 chai.use(chaiHttp);
 
 describe('Flow: create post', () => {
+  const { app } = new MydriaApp();
+  const userId = 'noUserIdWasSetForCurrentTest'
+  let xAccessToken;
+
+  before(async () => {
+    const signinForm = {
+      email: 'jane.doe@email.com',
+      password: 'janepass123',
+    }
+    const loginRequest = await chai.request(app)
+      .post('/signin').send(signinForm)
+    xAccessToken = loginRequest.text;
+  })
+
   xit('should create post', async () => {
-    const { app } = new MydriaApp();
     const postForm = {
-      user: 'noUserIdWasSetOnCurrentTest',
+      user: userId,
       body: {
         text: 'This is a post',
       },
     };
     const res = await chai.request(app)
-      .post(`/user/${postForm.user}/posts`).send(postForm);
+      .post(`/user/${postForm.user}/posts`).set('x-access-token', xAccessToken).send(postForm);
     expect(res).to.have.status(201);
   });
 });
