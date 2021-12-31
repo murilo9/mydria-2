6f906baedac6a91b1c08461249610c6db003c69c
murilo <murilohenriquematias@gmail.com>
2021-12-30 21:56:52 -0400

    side column on ProfileLayout

--- app/src/features/profiles/layouts/ProfileLayout.tsx
+++ app/src/features/profiles/layouts/ProfileLayout.tsx
@@ -3,10 +3,11 @@ import React, { ReactElement } from 'react';
 
 type ProfileLayoutProps = {
   detailsColumn: ReactElement,
-  postsList: ReactElement
+  postsList: ReactElement,
+  sideColumn: ReactElement
 }
 
-const ProfileLayout: React.FC<ProfileLayoutProps> = ({ detailsColumn, postsList }: ProfileLayoutProps) => {
+const ProfileLayout: React.FC<ProfileLayoutProps> = ({ detailsColumn, postsList, sideColumn }: ProfileLayoutProps) => {
   return <>
     <Grid container sx={{ pt: 3, px: { sm: 6, md: 4 } }} spacing={3}>
       <Grid item xs={12} md={4} lg={3}>
@@ -14,6 +15,9 @@ const ProfileLayout: React.FC<ProfileLayoutProps> = ({ detailsColumn, postsList 
       </Grid>
       <Grid item xs={12} md={8} lg={6}>
         {postsList}
+      </Grid>
+      <Grid item xs={0} lg={3}>
+        {sideColumn}
       </Grid>
     </Grid>
   </>
